7116405e9f3817b8a0d0b0705696d862ec83eaad
EC2 Default User <ec2-user@ip-10-0-1-219.ap-south-1.compute.internal>
2026-03-30 12:49:21 +0000

    Updates in Application

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from ECS Fargate ");
+  res.end("Succesfully Deployed Application through Pipeline using  ECS Fargate ");
 });
 
 server.listen(3000);
